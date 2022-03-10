f3d0727abc8d2a2d6b7487691017de9b76e27c11
EC2 Default User <ec2-user@ip-172-31-38-45.ec2.internal>
2022-03-10 10:06:37 +0000

    Add user,book各ページ編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,22 @@ class UsersController < ApplicationController
   end
 
   def index
+    @users = User.all
+    @book.user_id = current_user.id
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update
+    redirect_to user_path(@user.id)
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
   end
 end
