1a4c75ea41eb20a237e88b6b5da7ff46817b9813
EC2 Default User <ec2-user@ip-172-31-38-45.ec2.internal>
2022-03-11 09:07:12 +0000

    Add validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,9 @@ class User < ApplicationRecord
 
   has_one_attached :profile_image
   has_many :books, dependent: :destroy
+  
+  validates :name, presence: true, uniquness: true, length: { minimum: 2, maximum: 20}
+  validates :introduction, length: {maximum: 50}
 
   def get_profile_image(width, height)
     unless profile_image.attached?
