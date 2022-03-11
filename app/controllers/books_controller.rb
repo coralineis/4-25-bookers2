45d7244fbfc115d9a7fd15d8273a8542af53c132
EC2 Default User <ec2-user@ip-172-31-38-45.ec2.internal>
2022-03-11 07:53:42 +0000

    Add 投稿成功,更新flash

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,8 +2,13 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path(@book.id)
+      if book.save
+        flash[:success] = "You have created book successfully."
+        redirect_to book_path(@book.id)
+      else
+        @books = Book.all
+        render :index
+      end
   end
 
   def index
@@ -23,8 +28,12 @@ class BooksController < ApplicationController
 
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
-    redirect_to book_path(@book.id)
+      if @book.update(book_params)
+        flash[:success] = "You have updated book successfully."
+        redirect_to book_path(@book.id)
+      else
+        render :edit
+      end
   end
 
   def destroy
