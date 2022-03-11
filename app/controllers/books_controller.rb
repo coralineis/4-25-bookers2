ce3c3ec7b7ffee0051294b891e389ff3c05c9735
EC2 Default User <ec2-user@ip-172-31-38-45.ec2.internal>
2022-03-11 10:31:14 +0000

    Add アクセス制限

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -24,6 +24,9 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[id])
+    unless @book.user != current_user
+      redirect_to book_path(@book.id)
+    end
   end
 
   def update
